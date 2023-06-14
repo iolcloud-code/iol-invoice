11ff91eaf133e15539e3bdc22696c43ae723f24f
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2023-06-14 18:47:19 +0200

    added render db

--- migrations/versions/8fb680272d67_.py
+++ migrations/versions/8fb680272d67_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0d9117df9029
+Revision ID: 8fb680272d67
 Revises: 
-Create Date: 2023-06-14 16:15:16.003527
+Create Date: 2023-06-14 18:45:48.944688
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0d9117df9029'
+revision = '8fb680272d67'
 down_revision = None
 branch_labels = None
 depends_on = None
